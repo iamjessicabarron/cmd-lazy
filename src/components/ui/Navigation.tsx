4b0b89bedeb187ec93ff18e6a31a79f9776658b5
Jessica Barron <iamjessicabarron@gmail.com>
2023-09-19 22:37:48 +0100

    use daisyUI, add icons

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/logo.svg";
 
 const Navigation: FunctionComponent = () => {
   return (
-    <div className="w-full p-2 bg-white">
+    <div className="navbar bg-base-100">
       <img className="h-16 mx-auto" src={Logo} alt="logo" />
     </div>
   );
